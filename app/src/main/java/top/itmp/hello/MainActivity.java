258b4027a1e4356d0252b60515da3e21244cdf5f
leehz <hz@huaixiaoz.com>
2015-12-09 17:35:08 +0800

    update ui with java code not xml

--- app/src/main/java/top/itmp/hello/MainActivity.java
+++ app/src/main/java/top/itmp/hello/MainActivity.java
@@ -1,13 +1,56 @@
 package top.itmp.hello;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT);
+        relativeLayout.setLayoutParams(layoutParams);
+
+        super.setContentView(relativeLayout);
+
+        TextView textView = new TextView(this);
+        RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams1.addRule(RelativeLayout.ALIGN_PARENT_START);
+        layoutParams1.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        textView.setLayoutParams(layoutParams1);
+        textView.setTextColor(Color.CYAN);
+        textView.setTextSize(44);
+        textView.setText("hello world!!");
+        relativeLayout.addView(textView);
+
+        Button button = new Button(this);
+        RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams2.addRule(RelativeLayout.CENTER_IN_PARENT);
+        button.setLayoutParams(layoutParams2);
+        button.setText("Button");
+        relativeLayout.addView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello world!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
